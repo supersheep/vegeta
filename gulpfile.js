2b39c5fba6400b3df1452dba66477dee55d2ec8e
supersheep <supersheepxjy@gmail.com>
2014-06-07 10:09:36 +0800

    done gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,55 +8,81 @@ var ttf2woff = require('gulp-ttf2woff');
 var template = require('gulp-template');
 var rename = require('gulp-rename');
 var stylus = require('gulp-stylus');
+var clean = require('gulp-clean');
 var Q = require('q');
 
-gulp.task('iconfont', function() {
-  var tplDeferred = Q.defer();
-  var stylusDeferred = Q.defer();
+var codepoints;
+var FONT_DIR = './css/fonts/';
 
-  gulp.src(['css/svg/*.svg'])
+gulp.task('svg', ['clean'], function() {
+  return gulp.src(['css/svg/*.svg'])
     .pipe(svgicons2svgfont({
       fontName: 'vegeta'
     }))
-    .on('codepoints', function(codepoints) {
-      var locals = {
-        codepoints: codepoints
-      };
-
-      gulp.src('css/vegeta.tpl')
-        .pipe(template(locals))
-        .pipe(rename({
-          extname: ".styl"
-        }))
-        .pipe(gulp.dest('./css/'))
-        .on('end', function() {
-          stylusDeferred.resolve();
-        });
-
-      gulp.src('index.tpl')
-        .pipe(template(locals))
-        .pipe(rename({
-          extname: ".html"
-        }))
-        .pipe(gulp.dest('./'))
-        .on('end', function() {
-          tplDeferred.resolve();
-        });
-
-
-      // Here generate CSS/SCSS  for your codepoints ...
+    .on('codepoints', function(cp) {
+      codepoints = cp;
     })
-    .pipe(svg2ttf())
-    .pipe(ttf2eot())
-    .pipe(gulp.dest('css/fonts/'));
-  return Q.all([tplDeferred.promise, stylusDeferred.promise]);
+    .pipe(gulp.dest(FONT_DIR))
 });
 
-gulp.task('stylus', ["iconfont"], function() {
+gulp.task('stylusTpl', ['svg'], function() {
+  return gulp.src('css/vegeta.tpl')
+    .pipe(template({
+      codepoints: codepoints
+    }))
+    .pipe(rename({
+      extname: ".styl"
+    }))
+    .pipe(gulp.dest('./css/'));
+
+});
+
+gulp.task('pageTpl', ['svg'], function() {
+  return gulp.src('index.tpl')
+    .pipe(template({
+      codepoints: codepoints
+    }))
+    .pipe(rename({
+      extname: ".html"
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('ttf', ['svg'], function() {
+  return gulp.src('css/fonts/*.svg')
+    .pipe(svg2ttf())
+    .pipe(gulp.dest(FONT_DIR))
+});
+
+gulp.task('eot', ['ttf'], function() {
+  return gulp.src(['css/fonts/*.ttf'])
+    .pipe(ttf2eot())
+    .pipe(gulp.dest(FONT_DIR))
+});
+
+gulp.task('woff', ['ttf'], function() {
+  return gulp.src(['css/fonts/*.ttf'])
+    .pipe(ttf2woff())
+    .on('error', function(err) {
+      console.log(err);
+    })
+    .pipe(gulp.dest(FONT_DIR))
+});
+
+gulp.task('clean', function() {
+  return gulp.src(['css/fonts', 'css/vegeta.styl', 'css/vegeta.css', 'index.html'], {
+      read: false
+    })
+    .pipe(clean())
+});
+
+gulp.task('iconfont', ['eot', 'woff']);
+
+gulp.task('stylus', ["stylusTpl"], function() {
   gulp.src(['css/vegeta.styl'])
     .pipe(stylus())
     .pipe(gulp.dest('css/'))
 });
 
 
-gulp.task("default", ["stylus"]);
+gulp.task("default", ["stylus", "iconfont", "pageTpl"]);
